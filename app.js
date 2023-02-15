7bd778e3c536e788b0283a5fac87f94a5a14aaeb
midCenturyDude7 <mgriffes@gmail.com>
2023-02-14 22:04:02 -0800

    Mode is point to and persisting in localStorage()

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // PERSISTANT DARKMODE | BUILT WITH HTML, SCSS & JS
 const toggleBtn = document.getElementById("toggle-btn");
-const wrapper = document.querySelector(".wrapper");
+let element = document.body;
 let lightMode = localStorage.getItem("light-mode");
 
 function enableLightMode() {
-  wrapper.classList.add("light-mode-theme");
+  element.classList.add("light-mode-theme");
   toggleBtn.classList.remove("light-mode-toggle");
   localStorage.setItem("light-mode", "enabled");
 }
 
 function disableLightMode() {
-  wrapper.classList.remove("light-mode-theme");
+  element.classList.remove("light-mode-theme");
   toggleBtn.classList.add("light-mode-toggle");
   localStorage.setItem("light-mode", "disabled");
 }
