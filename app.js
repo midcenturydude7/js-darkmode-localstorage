c4528d2018989304c97b6138c0f105b86faa907b
midCenturyDude7 <mgriffes@gmail.com>
2023-02-14 17:08:25 -0800

    Able to persist mode - light or dark - on page load/render

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // PERSISTANT DARKMODE | BUILT WITH HTML, SCSS & JS
+const toggleBtn = document.getElementById("toggle-btn");
+const wrapper = document.querySelector(".wrapper");
+let lightMode = localStorage.getItem("light-mode");
+
+function enableLightMode() {
+  wrapper.classList.add("light-mode-theme");
+  toggleBtn.classList.remove("light-mode-toggle");
+  localStorage.setItem("light-mode", "enabled");
+}
+
+function disableLightMode() {
+  wrapper.classList.remove("light-mode-theme");
+  toggleBtn.classList.add("light-mode-toggle");
+  localStorage.setItem("light-mode", "disabled");
+}
+
+if (lightMode === "enabled") {
+  enableLightMode();
+}
+
+toggleBtn.addEventListener("click", () => {
+  lightMode = localStorage.getItem("light-mode"); // Update to LightMode when clicked
+  if (lightMode === "disabled") {
+    enableLightMode();
+  } else {
+    disableLightMode();
+  }
+});
