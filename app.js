4229907b8dd3d4fca5bae70ba2fd9de7a8844b75
midCenturyDude7 <mgriffes@gmail.com>
2023-02-16 10:31:13 -0800

    Cleaned up comments and added moon icon for lightMode theme

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 // PERSISTANT DARKMODE | BUILT WITH HTML, SCSS & JS
 const toggleBtn = document.getElementById("toggle-btn");
 const theme = document.getElementById("theme");
-// let element = document.body; // select the entire body
+const iconTxt = document.getElementById("icon-text");
 let lightMode = localStorage.getItem("light-mode");
 let darkMode = localStorage.getItem("dark-mode");
 
 function enableLightMode() {
   theme.classList.add("light-mode-theme"); // replaces darkMode at the :root
-  // toggleBtn.classList.remove("light-mode-toggle");
   theme.classList.remove("dark-mode-theme");
-  // toggleBtn.classList.add("dark-mode-toggle");
+  iconTxt.classList.replace("fa-brightness-low", "fa-moon");
   localStorage.setItem("light-mode", "enabled");
   localStorage.setItem("dark-mode", "disabled");
 }
 
 function disableLightMode() {
   theme.classList.remove("light-mode-theme");
-  // toggleBtn.classList.add("light-mode-toggle");
   theme.classList.add("dark-mode-theme");
-  // toggleBtn.classList.remove("dark-mode-toggle");
+  iconTxt.classList.replace("fa-moon", "fa-brightness-low");
   localStorage.setItem("light-mode", "disabled");
   localStorage.setItem("dark-mode", "enabled");
 }
