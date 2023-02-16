e2d4d787df14c76c8797e8b2b06cadefd749d574
midCenturyDude7 <mgriffes@gmail.com>
2023-02-15 20:10:54 -0800

    Resolved bug related to icon color and theme toggle

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // PERSISTANT DARKMODE | BUILT WITH HTML, SCSS & JS
 const toggleBtn = document.getElementById("toggle-btn");
-let element = document.body; // select the entire body
+const theme = document.getElementById("theme");
+// let element = document.body; // select the entire body
 let lightMode = localStorage.getItem("light-mode");
+let darkMode = localStorage.getItem("dark-mode");
 
 function enableLightMode() {
-  element.classList.add("light-mode-theme"); // replaces darkMode at the :root
-  toggleBtn.classList.remove("light-mode-toggle");
+  theme.classList.add("light-mode-theme"); // replaces darkMode at the :root
+  // toggleBtn.classList.remove("light-mode-toggle");
+  theme.classList.remove("dark-mode-theme");
+  // toggleBtn.classList.add("dark-mode-toggle");
   localStorage.setItem("light-mode", "enabled");
+  localStorage.setItem("dark-mode", "disabled");
 }
 
 function disableLightMode() {
-  element.classList.remove("light-mode-theme");
-  toggleBtn.classList.add("light-mode-toggle");
+  theme.classList.remove("light-mode-theme");
+  // toggleBtn.classList.add("light-mode-toggle");
+  theme.classList.add("dark-mode-theme");
+  // toggleBtn.classList.remove("dark-mode-toggle");
   localStorage.setItem("light-mode", "disabled");
+  localStorage.setItem("dark-mode", "enabled");
 }
 
 if (lightMode === "enabled") {
@@ -23,3 +31,5 @@ toggleBtn.addEventListener("click", () => {
   lightMode = localStorage.getItem("light-mode"); // Update to lightMode when clicked
   lightMode === "disabled" ? enableLightMode() : disableLightMode(); // Checks mode
 });
+console.log(`Lightmode is: ${lightMode}`);
+console.log(`Darkmode is: ${darkMode}`);
