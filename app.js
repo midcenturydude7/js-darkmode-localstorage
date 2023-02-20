12154ee65d47c6e5d33bd57ed59f36049f995754
midCenturyDude7 <mgriffes@gmail.com>
2023-02-19 21:01:36 -0800

    Added menu slide functionality

--- app.js
+++ app.js
@@ -36,18 +36,19 @@ let open = get("menu-btn");
 let navBar = get("navbar-responsive");
 let navTitle = get("nav-title");
 let exit = get("exit-btn");
+let header = get("header");
 
 open.addEventListener("click", () => {
-  // navBar.style.display = "flex";
   navBar.classList.add("navbar-responsive-open");
+  header.style.height = "15em";
   navTitle.style.display = "none";
   open.style.display = "none";
   exit.style.display = "block";
 });
 
 exit.addEventListener("click", () => {
-  // navBar.style.display = "none";
-  navBar.classList.remove("navbar-responsive-open");
+  navBar.classList.replace("navbar-responsive-open", "hideNav");
+  header.style.height = "7.6275em";
   navTitle.style.display = "block";
   open.style.display = "block";
   exit.style.display = "none";
